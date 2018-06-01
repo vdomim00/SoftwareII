fb907951a602483582d2a00899bb26a5d3217599
Victor Dominguez <riespri93@gmail.com>
2018-06-01 20:13:39 +0200

    all

---
+++ software/target/generated-sources/annotations/com/mycompany/model/DetallePedidosAAlmacen_.java
@@ -0,0 +1,15 @@
+package com.mycompany.model;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-06-01T20:11:48")
+@StaticMetamodel(DetallePedidosAAlmacen.class)
+public class DetallePedidosAAlmacen_ { 
+
+    public static volatile SingularAttribute<DetallePedidosAAlmacen, String> productos_codigoProducto;
+    public static volatile SingularAttribute<DetallePedidosAAlmacen, Integer> pedidoAlmacen_idPedido;
+    public static volatile SingularAttribute<DetallePedidosAAlmacen, Integer> cantidad;
+
+}
